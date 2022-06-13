e03140b6a1da371883a87ccb67e8dc3b9ca7195f
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-06-13 23:41:23 +0600

    🛠️📕🦄completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { AppContainer } from "./style";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(0);
+
+  const testimonials = [
+    {
+      name: "Tanya Sinclair",
+      role: "UX Engineer",
+      testimonial:
+        " I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ",
+      image: "/image-tanya.png",
+    },
+    {
+      name: "John Tarkpor",
+      role: "Junior Front-end Developer",
+      testimonial:
+        " If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ",
+      image: "/image-john.png",
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <main>
+        <div className="left">
+          <q>{testimonials[isOpen].testimonial}</q>
+          <div className="left_designation">
+            <p className="left_designation_name">{testimonials[isOpen].name}</p>
+            <p className="left_designation_role">{testimonials[isOpen].role}</p>
+          </div>
+          <img
+            src="/pattern-quotes.svg"
+            alt="quote illustration"
+            className="left_illus"
+          />
+        </div>
+        <div className="right">
+          <img
+            src={testimonials[isOpen].image}
+            alt={testimonials[isOpen].name}
+          />
+          <div className="right_shift">
+            <div
+              className="right_shift__button"
+              onClick={() =>
+                isOpen === 0
+                  ? setIsOpen(testimonials.length - 1)
+                  : setIsOpen(isOpen - 1)
+              }
+            >
+              <img
+                src="/icon-prev.svg"
+                alt="previous testimonial"
+                className="right_shift__button_image_prev"
+              />
+            </div>
+            <div
+              className="right_shift__button"
+              onClick={() =>
+                isOpen === testimonials.length - 1
+                  ? setIsOpen(0)
+                  : setIsOpen(isOpen + 1)
+              }
+            >
+              <img
+                src="/icon-next.svg"
+                alt="next testimonial"
+                className="right_shift__button_image_next"
+              />
+            </div>
+          </div>
+        </div>
+      </main>
+    </AppContainer>
   );
 }
 
